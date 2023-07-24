433a2492d673330485b2e7795aa9fcb142a03ee7
Pratikjadhav123 <jadhavpc1999@gmail.com>
2023-07-24 18:30:51 +0530

    React_Educational_Website Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom"
+import "./App.css";
+import Header1 from './components/common/heading/Header1';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import CourseHome from './components/allcourses/CourseHome';
+import Team from './components/team/Team';
+import Price from './components/pricing/Price';
+import Blog from './components/blog/Blog';
+import Contact from './components/contact/Contact';
+import Footer from './components/common/footer/Footer';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <Router>
+        <Header1 />
+        <Routes>
+         <Route path='/' exact Component={Home} />
+         <Route path='/about' exact Component={About} />
+         <Route path='/courses' exact Component={CourseHome} />
+         <Route path='/team' exact Component={Team} />
+         <Route path='/price' exact Component={Price} />
+         <Route path='/journal' exact Component={Blog} />
+         <Route path='/contact' exact Component={Contact} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
